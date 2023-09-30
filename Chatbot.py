9e04ba389e343b9a9f310fc9f1eb0f0d78f8e730
botidinamix <josedcape@gmail.com>
2023-09-29 21:58:45 -0500

    modificar Chatbot.py~

---
+++ Chatbot.py
@@ -0,0 +1,10 @@
+def main():
+    mensaje = input("Usuario: ")
+  
+    if "hola" in mensaje.lower():
+        print("Chatbot: Hola, bienvenido!")
+    else:
+        print("Chatbot: correcto ")
+    
+if __name__ == "__main__":
+    main()
